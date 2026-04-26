b0f9ab502295eb1b4fdac9fea6b956c70f7897a6
Alessandro D'Amico <damico_alex@hotmail.com>
2026-04-26 10:13:37 +0200

    fix CreateHintDto

--- be/src/main/java/dev/alessandrodamico/addio_celibrato/dtos/CreateHintDto.java
+++ be/src/main/java/dev/alessandrodamico/addio_celibrato/dtos/CreateHintDto.java
@@ -6,7 +6,7 @@ public record CreateHintDto
         (String content, Long questionId)
 {
     public static Hint fromDto(CreateHintDto dto) {
-        return new Hint(null, null, dto.content(), false,
+        return new Hint(null, null, dto.content(), true,
                 dto.questionId());
     }
 }
